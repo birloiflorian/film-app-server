ecd7db0cb66ca1b181096efb690f28efc91bebe7
Florian Birloi <florian.birloi@ro.ibm.com>
2019-01-30 12:17:15 +0200

    Add login and add mock user routes

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
-var User = mongoose.model('User', {
+
+var UserSchema = new mongoose.Schema({
     username: {
         required: true,
         type: String,
@@ -9,23 +12,73 @@ var User = mongoose.model('User', {
         maxlength: 8
     },
     email: {
-        required: true,
         type: String,
         trim: true,
-        maxlength: 30
+        maxlength: 30,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
     },
     password: {
         type: String,
         trim: true
     },
-    token: {
-        type: String,
-        trim: true
-    },
-    token_expiration_time: {
-        type: Number,
-        trim: true
-    }
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-module.exports = { User };
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, 'abc123').toString();
+
+    user.tokens = [{
+        access,
+        token
+    }];
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+UserSchema.statics.findByCredentials = function (username, password) {
+    var User = this;
+
+    return User.find().then((docs) => {
+        const user = docs[0];
+
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            // Use bcrypt.compare to compare password and user.password
+            if (user.password == password) {
+                resolve(user);
+            } else {
+                reject();
+            }
+        });
+
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {
+    User
+};
--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-var {mongoose} = require('./db/mongoose');
-var {ObjectID} = require('mongodb');
+var {
+    mongoose
+} = require('./db/mongoose');
+var {
+    ObjectID
+} = require('mongodb');
 var bodyParser = require('body-parser');
+const _ = require('lodash');
 
 
-var {User} = require('./models/user');
+var {
+    User
+} = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -16,14 +23,43 @@ app.get('/', (req, res) => {
 });
 
 app.post('/user', (req, res) => {
-    res.status(200).send({ text: "Create user!"});
+
+    const user = new User({
+        username: 'Anonymus',
+        password: 'Test123#',
+        email: 'test@email.com',
+    })
+
+    user.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-app.get('/movies', (req, res) => {
-    console.log("Get data");
+app.get('/user', (req, res) => {
+    User.find().then((docs) => {
+        res.send(docs);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
+
+app.post('/login', (req, res) => {
+    var body = _.pick(req.body, ['username', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        // console.log('user', user);
+        return user.generateAuthToken().then((token) => {
+            res.send(user);
+        }, (err) => {
+            res.status(400).send(err);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+})
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
-
